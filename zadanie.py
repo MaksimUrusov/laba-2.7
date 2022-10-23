d11dbaf0bafaf7bd970c84003c547a79e4275e34
MaksimUrusov <den-ney@bk.ru>
2022-10-24 00:45:33 +0300

    test

---
+++ zadanie.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    # 1
+    str_in = input("Введите строку: ").lower()
+    a = set(str_in.replace(' ', ''))
+    vowels = set("aeiouyаоуыэеёиюяи")
+    count = 0
+    for i in str_in:
+        if i in vowels:
+            count += 1
+    print('Количество глассных в предложении = ', count)
+
+    # 2
+    a = input('Введите строку\n')
+    s1 = set(a.replace(' ', ''))
+    a = input('Введите строку\n')
+    s2 = set(a.replace(' ', ''))
+    print('Количество общих символов = ', s1.intersection(s2))
