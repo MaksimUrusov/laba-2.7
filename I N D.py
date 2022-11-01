cf01c1386c1cd94885215d1a23e2fe0168f7702e
MaksimUrusov <den-ney@bk.ru>
2022-11-01 11:30:03 +0300

    ind1

--- I N D.py
+++ I N D.py
@@ -1,37 +1,16 @@
 #Пер Об Разн
 
-if __name__ == '__main__':
-    b = input('b = ')
-    c = input('c = ')
-    g = input('g = ')
-    I = input('I = ')
-    w = input('w = ')
-    e = input('e = ')
-    h = input('h = ')
-    q = input('q = ')
-    d = input('d = ')
-    k = input('k = ')
-    l = input('l = ')
-    y = input('y = ')
-    a = input('a = ')
-    u = input('u = ')
-    v = input('v = ')
-    z = input('z = ')
-
-
-    mn_a = {b, c, g, I, w}
-    mn_b = {e, g, h, q, w}
-    mn_c = {c, d, k, l, y}
-    mn_d = {a, g, h, u, v, z}
-    mn_u = set("abcdefghijklmnopqrstuvwxyz")
-    a = mn_u.difference(mn_a)
-    print('Множество A = ', mn_a)
-    print('Множество B = ', mn_b)
-    print('Множество C = ', mn_c)
-    print('Множество D = ', mn_d)
-
-    mn_x = (mn_a.intersection(mn_c)).union(mn_b)
-    print('\nМножество X = ', mn_x)
-
-    mn_y =(a.intersection(mn_d)).union(mn_c.difference(mn_b))
-    print('Множество Y = ', mn_y)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == "__main__":
+    u = set("abcdefghijklmnopqrstuvwxyz")
+    a = {"b", "c", "g", "I", "w"}
+    b = {"e", "g", "h", "q", "w"}
+    c = {"c", "d", "k", "l", "y"}
+    d = {"a", "g", "h", "u", "v", "z"}
+    x = (a.intersection(c)).union(b)
+    print(f"x = {x}")
+    # Найдем дополнения множеств
+    an = u.difference(a)
+    y = (an.difference(d)).union(c.difference(b))
+    print(f"y = {y}")
--- zd1.py
+++ zd1.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 if __name__ == '__main__':
     # 1
     str_in = input("Введите строку: ").lower()
@@ -14,4 +17,4 @@ if __name__ == '__main__':
     s1 = set(a.replace(' ', ''))
     a = input('Введите строку\n')
     s2 = set(a.replace(' ', ''))
-    print('Количество общих символов = ', s1.intersection(s2))
+    print('Количество общих символов = ', s1.intersection(s2)
